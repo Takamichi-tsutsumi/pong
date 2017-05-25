2916eb982cbc13624f010d83588f026a36febd67
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-05-26 00:10:31 +0900

    hyper sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate hyper;
+
+use std::io::Write;
+use std::sync::Mutex;
+use std::sync::mpsc::{channel, Sender};
+use hyper::server::{Handler, Server, Request, Response};
+
+struct SenderHandler {
+    sender: Mutex<Sender<&'static str>>,
+}
+
+impl Handler for SenderHandler {
+    fn handle(&self, req: Request, mut res: Response) {
+        println!("Recieved Request");
+        self.sender.lock().unwrap().send("start").unwrap();
+        let body = b"Pong";
+
+        let mut res = res.start().unwrap();
+        res.write_all(body).unwrap();
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = channel();
+
+    Server::http("127.0.0.1:9000")
+        .unwrap()
+        .handle(SenderHandler { sender: Mutex::new(tx) })
+        .unwrap();
 }
